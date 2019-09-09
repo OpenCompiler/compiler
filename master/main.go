482701f999930b6cc051697e4d5593a5fdc762ce
Kitamura Yuta <me@ugwis.net>
2019-09-09 11:15:35 +0900

    Add WithAnalytics

--- master/main.go
+++ master/main.go
@@ -56,7 +56,7 @@ func main() {
 		os.Getenv("DD_AGENT_HOST"),
 		os.Getenv("DD_TRACE_AGENT_PORT"),
 	)
-	tracer.Start(tracer.WithAgentAddr(addr))
+	tracer.Start(tracer.WithAgentAddr(addr), tracer.WithAnalytics(true))
 	defer tracer.Stop()
 	ctx := context.Background()
 
